711da36ee0a12369b80d647a593ebba66f65e66b
stricovtcov <strik85@tut.by>
2020-10-27 22:23:00 +0300

    ty commit

---
+++ java.js
@@ -0,0 +1,15 @@
+// const users = [1,2,3,4,5,6,7];
+// const someFunc = (number) => {
+// if (!Array.isArray(number)) return;
+// return number.map(x =>x*2)
+// }
+// console.log(someFunc(users));
+// const users = [1.3,2,3,4,5,6,7,9];
+// const someFunc = (number) => {
+//  if (!Array.isArray(number)) return;
+//  return number.map ((x,y,z) =>{
+//    (y==0 || y== users.length-1) ? x=x**2 : x=x+1;
+// return x
+//   })
+// }
+// console.log(someFunc(users));
